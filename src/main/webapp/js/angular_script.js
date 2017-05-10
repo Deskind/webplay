8d63d8cecfe3efafa23a1664ef254053954c367b
AUTO <AUTO@Andrey>
2017-05-10 16:07:50 +0200

    $http service and sign in form

--- src/main/webapp/js/angular_script.js
+++ src/main/webapp/js/angular_script.js
@@ -6,19 +6,24 @@ app.config(function($routeProvider) {
     })
     .when("/login", {
         templateUrl : "views/signInForm.html"
-    })
-    .when("/green", {
-        templateUrl : "green.html"
-    })
-    .when("/blue", {
-        templateUrl : "blue.html"
     });
 });
 
-var controller = app.controller('ctrl', function ($scope, $location){
+var controller = app.controller('ctrl', function ($scope, $location, $http){
     $scope.changeLocToSignIn = function (){
         console.log($location.path());
         $location.path("/login");
         console.log($location.path());
     };
+    $scope.submitSignIn = function (){
+        console.log("Trying to get data from server");
+        $http({
+            method: 'GET',
+            url: 'loginServlet?signName='+$scope.signName+'&signPassword='+$scope.signPassword
+            }).then(function successCallback(response) {
+              console.log(response.data);
+            }, function errorCallback(response) {
+              console.log('Something went wrong!');
+            });
+        };
 });
